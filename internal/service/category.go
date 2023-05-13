6cce74ebee224ec4b66858eaeea6517093d575fd
salesof7 <jpsaless2002@gmail.com>
2023-05-13 00:35:08 -0300

    feat: implement service of CreateCategory with stream

--- internal/service/category.go
+++ internal/service/category.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/salesof7/studies_go-gRPC/internal/database"
 	"github.com/salesof7/studies_go-gRPC/internal/pb"
@@ -66,4 +67,29 @@ func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetReq
 	}
 
 	return categoryResponse, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: *categoryResult.Description,
+		})
+	}
 }
